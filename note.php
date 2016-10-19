625e7f7aedfcae8a7b847215c7e714cb834759e7
root <reddeveloppement@wanalike.fr>
2016-10-19 22:31:58 +0200

    upload my Cv

---
+++ note.php
@@ -0,0 +1,46 @@
+<div class="col-lg-5">
+					<h4>Experiences </h4>
+					
+					<img src="img/" />
+					<h4>2tech</h4>
+					<p>2016</p>
+					<p>Developpeur php</p>
+					<p> Gestion de stock en php/sql</p>
+				
+					<img src="img/" />
+					<h4>Ebulition.fr</h4>
+					<p>2016<p>
+					<p>Site Vitrine Vente de vetements</p>
+				
+					<img src="img/" />
+					<h4>wanahost.fr </h4>
+					<p>Developpeur back-end</p>
+					<p>plateforme d'hebergement associative bretonne</p>
+					</div>
+					<div class="col-lg-5">
+					<h4> Project en cours</h4>
+					<img src="img/logo.png" />
+					<p>Proprietaire et Developpeur à Shambhalafm.fr<p>
+					<p>2015 a maintenant</p>
+					<p>Web Radio OpenSource</p>
+				
+					<img src="img/" />
+					<h4>Developper front a wanalike.fr<h4>
+					<p>2015 a maintenant<p>
+					<p>Site de rencontre et de partage </p>
+
+
+					<img src="img/" />
+					<p>2015 a maintenant<p>
+					<p> Develloppeur à CalinouPeluche-C&A.fr</p>
+					<p>Histoire de web - Histoire numerique et ludique<p>
+					</div>
+				
+				<p class="col-lg-4"> 
+					Vaupré Julien<br />
+					.CACESS1/3/5<br /> 
+					68 RUE Saint-Hovec<br /> 
+					22600 LOUDEAC<br /> 
+					0602074343<br />
+					lesonduko@gmail.com
+				</p>
