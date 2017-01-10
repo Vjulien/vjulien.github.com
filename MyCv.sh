d995657f0b0a851689c74c4c4beead5cae60d37c
JulienV <reddeveloppement@wanalike.fr>
2017-01-10 21:26:56 +0000

    work in progress on Mycv.sh and rewrite index.html (Fondateur)

--- MyCv.sh
+++ MyCv.sh
@@ -23,19 +23,55 @@ fi
 
 
 
-sleep 2
+sleep 1
 
 printf -- '%s\n' "Plus qu'une passion un mode de vie  "
 
+sleep 1
 
 
-printf -- '%s\n' "Autodidacte et passioné depuis 10 ans  "
+printf -- '%s\n' "Autodidacte et passioné depuis une 10ene d'années  "
 
-printf -- '%s\n' "Retrouvez moi Sur github: Vjulien"
+sleep 1
 
-sleep 2 
 
-printf -- '%s\n' " " 
+printf -- '%s\n' "Retrouvez moi Sur github: https://github.com/Vjulien"
+
+sleep 1 
+
+printf -- '%s\n' "Fondateur et Developper a Shambhalafm.fr"
+
+sleep 1
+
+printf -- '%s\n' "Fondateur et Developper a Shambhalafm.fr" 
+
+sleep 1
+
+printf -- '%s\n' "C'est à la fin de l'année 2015 que le projet a été mit en place, avec plaisir et détermination, sur des sujets proposés par les auditeurs." 
+
+sleep 1
+
+printf -- '%20s' "Toujours en Développement, le projet est conçu en php/sql, bootstrap4, css3 et html5.
+                         Je m'emploie à assurer la continuité et le maintien du développement du projet, une version Alpha est en cours." 
+
+
+#logo1="/usr/bin/linuxlogo" 
+#logo2="/usr/bin/X11/linuxlogo"
+#logo3="/usr/share/man/man1/linuxlogo.1.gz" 
+#if [ -f $logo1 ] || [ -f $logo2 ] || [ -f $logo3 ] ; then
+
+#linuxlogo
+#
+#else
+#
+# sudo apt-get install linuxlogo && linuxlogo
+#
+#fi
+#
+#linuxlogo 2>/dev/null && linuxlogo || echo "Mais ou est mon System préféré ?" && sudo apt-get install linuxlogo
+
+
+
 
 exit
 
