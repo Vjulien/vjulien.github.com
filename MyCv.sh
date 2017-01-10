256709d3b2681f9b2f70fd35bc32ed41c648da9f
Redfish <lesonduko@gmail.com>
2017-01-10 17:13:24 +0100

    add bash cv version

---
+++ MyCv.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+
+printf -- '%s\n' "Bienvenue Sur Mon Cv Programmé en Shell bash"
+sleep 1
+printf -- '%s\n' "Découvrez Mes Skill's"
+sleep 1
+printf -- '%s\n' "Du Php en passant par AngularJS Jusqu'au Sql "
+sleep 1
+printf -- '%s\n' "le métier d'une vie"
+
+train= sl  
+
+if test "$train"
+then 
+
+sl
+
+else
+
+ sudo apt-get install sl && sl
+
+fi
+
+
+sleep 2
+
+printf -- '%s\n' "Plus qu'une passion un mode de vie  "
+
+
+
+printf -- '%s\n' "Autodidacte et passioné depuis 10 ans  "
+
+printf -- '%s\n' "Retrouvez moi Sur github: Vjulien"
+
+sleep 2 
+
+printf -- '%s\n' " " 
+
+exit
+
+
+
+
+
