252bbfcea392507d5981a1132acfe8815487e82b
Redfish <lesonduko@gmail.com>
2017-01-16 17:22:22 +0100

    work in progress on MyCvArray.sh

--- MyCvArray.sh
+++ MyCvArray.sh
@@ -38,18 +38,27 @@ echo "                                                                          
 
 banner "MonCv" 
 
-x = 0
-y = arr
-$y$x=(Bienvenue Sur Mon Cv Programmé en Shell "bash")
-$y$x=("Découvrez Mes Skills Du Php en passant par AngularJS Jusquau Sql")
-$y$x=("le métier dune vie")
-array=($y$x)
+#GROUPE3="Bienvenue Sur Mon Cv Programmé en Shell 'bash'"
+#CPT=3
+#GRP=GROUPE$CPT
+#RES=${!GRP}
+#echo $RES
+
+
+
+
+x=0
+y=()
+y+=('Bienvenue Sur Mon Cv Programmé en Shell bash')
+y+=("Découvrez Mes Skills Du Php en passant par AngularJS Jusquau Sql")
+y+=("Le métier dune vie")
+
 #echo ${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]}
 
-for item in ${array[*]}
+for item in ${y[*]}
 do
-
-    printf "   %s\n" $item && $x++
+	sleep 0.5
+    printf "   %s" $item && $x + 1
 done
 
 
