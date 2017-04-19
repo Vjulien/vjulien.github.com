44d6eccaf666c239ef2454a678a3e0dc6c0495ba
redfish <redfishaw@gmail.com>
2017-04-19 19:17:26 +0200

    add popover and button in nav

---
+++ save.php
@@ -0,0 +1,59 @@
+<!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+
+
+
+
+
+        <h3>Qui suis-je ?</h3>
+            <p>Passioné depuis tout jeune, Au debut des années 2000,</p>
+            <p> je me suis interresser au fonctionnement
+                des machines, puis trés vite aux programmes et aux codes les composants</p>        
+            <p>Je commence le developpement par l'irc avec quelques scripts. </p>
+            <p>Viens ensuite le debut du html et du css,    
+             puis au fil du temp, je me suis pencher sur les languages machines</p>  
+             <p>A la sortie, de mon BEP electronique a default d'experiences je peine a trouver du travail,
+             je trouve donc un emploie de magasinier ou j'apprend sur le tas et passe par la suite le diplome</p>
+             <p>Apres plusieur années en tant que magasinier, je decide de me consacré a ma passion a 100% 
+             et integre la code academie pour obtenir la certification developpeur logiciel</p>
+
+
+<button type="button" class="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+  Popover on top
+</button>
+
+<button type="button" class="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="right" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+  Popover on right
+</button>
+
+<button type="button" class="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus
+sagittis lacus vel augue laoreet rutrum faucibus.">
+  Popover on bottom
+</button>
+
+<button type="button" class="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="left" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+  Popover on left
+</button>
